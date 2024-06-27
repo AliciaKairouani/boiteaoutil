e7d3343df6f70b16c96ccf865a58631efd5d703b
AliciaKairouani <104007602+AliciaKairouani@users.noreply.github.com>
2024-06-27 16:45:49 +0200

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,12 +1,10 @@
 from transformers import VitsModel, AutoTokenizer
 import torch
 from datasets import load_dataset
-import torch
-from IPython.display import Audio
 import os
 from dotenv import load_dotenv
 import streamlit as st
-import scipy
+import scipy.io.wavfile
 
 load_dotenv()
 api_key = os.getenv("HUGGINGFACE_API_KEY")
@@ -15,35 +13,34 @@ model = VitsModel.from_pretrained("facebook/mms-tts-fra")
 tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-fra")
 
 
-def synthesise(text,model):
+def synthesise(text, model):
     inputs = tokenizer(text, return_tensors="pt")
     with torch.no_grad():
         output = model(**inputs).waveform
     return output
 
 
-def process_input(user_input):
-    # Placez ici votre code pour traiter l'entrée utilisateur
-    return f"Vous avez entré : {user_input}"
-
 # Titre de l'application
-st.title("Du texte à la parol")
+st.title("Du texte à la parole")
 
 # Champ de saisie de texte
-user_input = st.text_input("Entrez quelque chose:")
+user_input = st.text_input("Entrez quelque chose:", key="text_input")
 
 # Bouton pour valider et lancer le processus
-if st.button("Valider"):
-    audio = synthesise(user_input,model)
-    scipy.io.wavfile.write("user.wav", rate=model.config.sampling_rate, data=audio)
+if st.button("Valider", key="validate_button"):
+    audio = synthesise(user_input, model)
+    scipy.io.wavfile.write("user.wav", rate=model.config.sampling_rate, data=audio.numpy())
     with open("user.wav", "rb") as file:
         audio_bytes = file.read()
+    
     # Afficher le lecteur audio
-    st.audio(audio_bytes, format='audio/wav')
+    st.audio(audio_bytes, format='audio/wav', key="audio_player")
+    
     # Bouton de téléchargement du fichier audio
     st.download_button(
         label="Télécharger le fichier audio",
         data=audio_bytes,
-        file_name=output_filename,
-        mime="audio/wav"
+        file_name="user.wav",
+        mime="audio/wav",
+        key="download_button"
     )
